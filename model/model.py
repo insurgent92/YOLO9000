940fe12252a3576d308d9caedf4ebfd39dac6373
visionNoob <insurgent92@gmail.com>
2019-02-24 16:52:32 +0900

    #31: Update model.py

--- model/model.py
+++ model/model.py
@@ -1,7 +1,8 @@
+from collections import OrderedDict
 import torch.nn as nn
 from torchsummary.torchsummary import summary
 from base import BaseModel
-
+from model import backbone
 
 class Yolo9000(BaseModel):
     def __init__(self,
@@ -9,7 +10,8 @@ class Yolo9000(BaseModel):
                  num_prior_boxes=5,
                  prior_boxes=None,
                  device="cpu",
-                 input_size=(416, 416)):
+                 input_size=(416, 416),
+                 pretrained_backbone=True):
         assert isinstance(num_prior_boxes, int)
         assert num_prior_boxes == len(prior_boxes)
         for box in prior_boxes:
@@ -25,83 +27,18 @@ class Yolo9000(BaseModel):
         self.num_prior_boxes = num_prior_boxes
         self.num_classes = num_classes
         self.output_shape = (self.num_coordinates + self.num_classes) * self.num_prior_boxes
+        
+        self.darknet19 = backbone.Darknet19(pretrained=pretrained_backbone)
 
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(),
-            nn.Conv2d(128, 64, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(),
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(),
-            nn.Conv2d(256, 128, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(),
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-
-        self.layer5 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(),
-            nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(),
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(),
-            nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(),
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-
-        self.layer6 = nn.Sequential(
-            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(1024),
-            nn.LeakyReLU(),
-            nn.Conv2d(1024, 512, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(),
-            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(1024),
-            nn.LeakyReLU(),
-            nn.Conv2d(1024, 512, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(),
-            nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(1024),
-            nn.LeakyReLU())
+        self.backbone = self.darknet19.features
 
         self.layer7 = nn.Sequential(
             nn.Conv2d(1024, 1024, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(1024),
             nn.LeakyReLU(),
             nn.Conv2d(1024, self.output_shape, kernel_size=1, stride=1, padding=0))
+
+        self.detector = nn.Sequential(OrderedDict([('layer7', self.layer7)]))
 
     def forward(self, *input):
         """
@@ -111,13 +48,8 @@ class Yolo9000(BaseModel):
         shape : [batch, n, n, 125]
         """
         x = input[0]
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = self.layer5(out)
-        out = self.layer6(out)
-        out = self.layer7(out)
+        out = self.backbone(x)
+        out = self.detector(out)
         out = self.post_processing(out)
 
         return out
